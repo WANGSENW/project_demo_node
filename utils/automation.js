73f498d478c92a2ff248d0920fbfb52d2dff8946
wangsen <15841621107m0@sina.cn>
2020-12-03 15:41:16 +0800

    修改地址

--- utils/automation.js
+++ utils/automation.js
@@ -2,6 +2,6 @@ module.exports = {
     USER: 'root',
     HOST: ['81.70.229.157'],
     REF: "origin/main",
-    REPO: 'git@github.com:WANGSENW/project_demo_node',
+    REPO: 'git@github.com:WANGSENW/project_demo_node.git',
     PATH:'/usr/local/myProject'
 }
